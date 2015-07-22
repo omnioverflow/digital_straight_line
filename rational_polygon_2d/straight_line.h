725365ca26fcd97c6cd2f5253d8928648acb44b0
iirthw <iirthw@gmail.com>
2015-07-22 16:23:33 +0200

    Added Hausdorff case

--- rational_polygon_2d/straight_line.h
+++ rational_polygon_2d/straight_line.h
@@ -1,18 +1,24 @@
 #pragma once
+#include"boost\numeric\ublas\matrix.hpp"
+#include <boost\rational.hpp>
 #include<complex>
 #include<fstream>
 #include<iostream>
+#include<string>
 #include<vector>
+
+using namespace boost::numeric::ublas;
 
 /* 
 Generate pixels for a straight line b*y = a*x + c, where c = 0.
 n is the cardinality of digitale straight line i.e. number of pixels in its representation
 */
-void generate_straight_line(int a, int b, int n) {
+void generate_straight_line(float a, float b, int n) {
 	std::vector<std::complex<int>> straight_line;
 	straight_line.reserve(n);
 	// Generate straight line (refer to course notes "Droites_Transparents")
-	int x = 0, y = 0, r = 0;
+	int x = 0, y = 0;
+	float r = 0.0f;
 	for(size_t i = 0; i < (n - 1); i++) {
 		if(r < b - a) {
 			r += a;
@@ -31,8 +37,30 @@ void generate_straight_line(int a, int b, int n) {
 	// Save segment to a text file
 	std::ofstream fileToSave;
 	fileToSave.open("segments/segment.txt");
+	fileToSave << straight_line.size() << std::endl;
 	for(size_t i = 0; i < straight_line.size(); i++) {
 		fileToSave << straight_line[i].real() << " " << straight_line[i].imag() << std::endl;
 	}
 	fileToSave.close();
+}
+
+matrix<int> *build_straight_line_from_file(std::string S) {
+	std::ifstream fileToRead;
+	fileToRead.open(S);
+	size_t numOfLines = 0;
+	fileToRead >> numOfLines;
+	
+	matrix<int> *M = new matrix<int>(numOfLines, 2);
+	//numOfLines++;
+
+	int x = 0, y = 0;
+	for(size_t i = 0; i < numOfLines; i++) {
+		fileToRead >> x >> y;
+		M->insert_element(i, 0, x);
+		M->insert_element(i, 1, y);
+	}
+
+	fileToRead.close();
+
+	return M;
 }
